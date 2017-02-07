d10a16040b732e6486ac39033f5b8b35a02052fb
jlrandulfe <jlrandulfe@gmail.com>
2017-02-07 11:00:42 +0100

    Corrected bug in list separator

--- conf.py
+++ conf.py
@@ -26,7 +26,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['cv2', 'rospy', 'geometry_msgs', 'geometry_msgs.msg', 'skimage' 
+MOCK_MODULES = ['cv2', 'rospy', 'geometry_msgs', 'geometry_msgs.msg', 'skimage', 
                 'skimage.measure', 'skimage.morphology', 'path_tracker']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
