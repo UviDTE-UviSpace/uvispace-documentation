8bb1259ed8589238d6d844a855b349314b8ff988
jlrandulfe <jlrandulfe@gmail.com>
2017-06-02 10:27:35 +0200

    Add matplotlib module to docs mock list
    The reason is that readthedocs raises errors when trying to compile the
    project due to the aforementioned module.

--- conf.py
+++ conf.py
@@ -26,8 +26,8 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return MagicMock()
 
-MOCK_MODULES = ['cv2', 'rospy', 'geometry_msgs', 'geometry_msgs.msg', 'skimage',
-                'skimage.measure', 'skimage.morphology', 'path_tracker']
+MOCK_MODULES = ['cv2', 'skimage', 'skimage.measure', 'skimage.morphology', 
+                'path_tracker', 'matplotlib', 'matplotlib.pyplot']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # -- General configuration ------------------------------------------------
